9b6a3d8189b2c441c1063fd19837504a3e9ceb69
Cryptotrolls75 <cryptotrolls@gmail.com>
2020-03-15 16:55:19 +0300

    0. Что такое переменная.[#255270]

---
+++ src/ru/job4j/Variable.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+public class Variable {         //ТИП_ДАННЫХ ИМЯ_ПЕРЕМЕННОЙ = ЗНАЧЕНИЕ_ПЕРЕМЕННОЙ;
+    public static void main(String[] args) {
+        byte mem = 1;           //используется для хранения байтовой информации в памяти. 8 bits.
+        short size = 1;         //целочисленный тип. 16 bits.
+        int lenght = 1;         //целочисленный тип. 32 bits.
+        long money = 1;         //целочисленный тип. 64 bits.
+        float size = 1.05;      //числа с плавающей точкой. 32 bits.
+        double size = 1000.02;  //числа с плавающей точкой. 64 bits.
+        boolean exists = true;  //описывают логический тип. 1 bit.
+        char exit = 'Y';        //символьный тип. 16 bits.
+    }
+    public static void lain(String[] args) {
+        int age = 18;
+        float length = 78.0332;
+        boolean baby = true;
+    }
+}
