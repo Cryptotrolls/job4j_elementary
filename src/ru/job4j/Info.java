e45022a1faa0b0b019fc54f2f04f5d64ce71c3f5
Cryptotrolls75 <cryptotrolls@gmail.com>
2020-03-15 15:49:21 +0300

    3.4. Заголовок в файле java[#255277]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("15.03.2020 15:38");
+        System.out.println("15.03.2020 15:38");   // почему то долго загружается на гитхаб
     }
 }
